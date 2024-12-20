82f27af4151266c7514160eb4f4a8ca6a1d882a5
vcosmin2701 <v.cosmin2717@gmail.com>
2024-12-20 23:12:16 +0200

    Added basic tests for this library

--- Tests/UnitConverterTests/UnitConverterTests.swift
+++ Tests/UnitConverterTests/UnitConverterTests.swift
@@ -1,1 +1,17 @@
+import XCTest
+@testable import UnitConverter
 
+final class ConverterTests: XCTestCase {
+    
+    func testLengthConversions() {
+        XCTAssertEqual(UnitConverter.metersToKilometers(1000), 1)
+    }
+    
+    func testWeightConversions() {
+        XCTAssertEqual(UnitConverter.kilogramsToPounds(1), 2.20462, accuracy: 0.0001)
+    }
+    
+    func testTemperatureConversions() {
+        XCTAssertEqual(UnitConverter.celsiusToFahrenheit(0), 32)
+    }
+}
