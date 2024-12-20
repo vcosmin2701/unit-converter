65e8ecb994429e7a1542397bfb90896fb56da575
vcosmin2701 <v.cosmin2717@gmail.com>
2024-12-20 23:07:53 +0200

    Added basic conversions

--- Sources/UnitConverter/UnitConverter.swift
+++ Sources/UnitConverter/UnitConverter.swift
@@ -1,1 +1,19 @@
+import Foundation
 
+public class UnitConverter {
+    
+    // MARK: - Length Conversion
+    public static func metersToKilometers(_ meters: Double) -> Double {
+        return meters / 1000
+    }
+    
+    // MARK: - Weight Conversion
+    public static func kilogramsToPounds(_ kilograms: Double) -> Double {
+        return kilograms * 2.20462
+    }
+    
+    // MARK: - Temperature Conversion
+    public static func celsiusToFahrenheit(_ celsius: Double) -> Double {
+        return (celsius * 9/5) + 32
+    }
+}
